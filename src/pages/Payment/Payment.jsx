cb56eadaeda36b8dc597fc82212087aeb9b70f47
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2024-10-12 11:30:57 +0900

    ✨:: payment 연결

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,12 +1,40 @@
-import React from "react";
-import { useLocation } from "react-router-dom"; // useLocation 훅 추가
+import React, { useEffect, useRef } from "react";
+import { useNavigate, useLocation } from "react-router-dom"; // useNavigate 훅 추가
 import * as P from "../../style/Payment";
 import nfc from "../../assets/img/nfc.svg";
 
 function Payment() {
   const location = useLocation(); // 이전 페이지에서 전달된 상태를 받음
+  const navigate = useNavigate(); // 페이지 전환을 위한 navigate 훅 사용
+  const ws = useRef(null); // WebSocket 참조
   const serverResponse =
     location.state?.serverResponse || "No message received"; // 서버 응답을 상태로 설정
+
+  useEffect(() => {
+    // WebSocket 연결 설정
+    ws.current = new WebSocket("ws://192.168.35.6:8050");
+
+    ws.current.onopen = () => {
+      console.log("WebSocket connected");
+    };
+
+    ws.current.onmessage = (event) => {
+      console.log("Message from server:", event.data);
+      if (event.data === "CARD:PASS") {
+        // CARD:PASS 메시지가 오면 PaymentComplete 페이지로 이동
+        navigate("/paymentComplete");
+      }
+    };
+
+    ws.current.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+
+    // 컴포넌트 언마운트 시 WebSocket 연결 종료
+    return () => {
+      if (ws.current) ws.current.close();
+    };
+  }, [navigate]);
 
   return (
     <>
